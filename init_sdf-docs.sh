508638795df22b60b4d20ff10d5d14f89c4fcfb5
Yee-Ting Li <yee379@gmail.com>
2022-03-08 15:55:58 -0800

    refactor: rename variables to make TMPDIR and SDF docs location more apparent

--- init_sdf-docs.sh
+++ init_sdf-docs.sh
@@ -2,20 +2,20 @@
 
 SLAC_SDF_DOCS_REPO=${SLAC_SDF_DOCS_REPO:-https://github.com/slaclab/sdf-docs.git}
 SLAC_SDF_DOCS_RELEASE=${SLAC_SDF_DOCS_VERSION:-master}
-SLAC_SDF_DOCS_PATH=${SLAC_SDF_DOCS_PATH:-/var/www/ood/public/doc}
+TMPDIR=${TMPDIR:-/stage}
 SLAC_SDF_DOCS_UPDATE_INTERVAL=${SLAC_SDF_DOCS_UPDATE_INTERVAL:-300}
-EMPTYDIR_MOUNT_PATH=/sdf-docs
+SLAC_SDF_DOCS_PATH=${SLAC_SDF_DOCS_PATH:-/sdf-docs}
 
 # always ensure there's some documentation, otherwise exit
 if [ ! -d "${SLAC_SDF_DOCS_PATH}" ] ; then
-    mkdir -p ${SLAC_SDF_DOCS_PATH} && cd ${SLAC_SDF_DOCS_PATH}
+    mkdir -p ${TMPDIR} && cd ${TMPDIR}
     git clone ${SLAC_SDF_DOCS_REPO}
-    cp -r ${SLAC_SDF_DOCS_PATH}/sdf-docs/. ${EMPTYDIR_MOUNT_PATH}
+    cp -r ${TMPDIR}/sdf-docs/. ${SLAC_SDF_DOCS_PATH}
 else
     exit 1
 fi
 
-cd ${EMPTYDIR_MOUNT_PATH}
+cd ${SLAC_SDF_DOCS_PATH}
 git checkout ${SLAC_SDF_DOCS_RELEASE}
 git fetch origin
 
