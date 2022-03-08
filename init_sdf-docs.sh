28657888c1730a5b90a1d5e0cb30f9b144e1c082
yee379 <yee379@gmail.com>
2022-03-08 15:49:01 -0800

    refactor: init empty, update/pull on destination

--- init_sdf-docs.sh
+++ init_sdf-docs.sh
@@ -2,20 +2,22 @@
 
 SLAC_SDF_DOCS_REPO=${SLAC_SDF_DOCS_REPO:-https://github.com/slaclab/sdf-docs.git}
 SLAC_SDF_DOCS_RELEASE=${SLAC_SDF_DOCS_VERSION:-master}
-SLAC_SDF_DOCS_PATH=/var/www/ood/public/doc
+SLAC_SDF_DOCS_PATH=${SLAC_SDF_DOCS_PATH:-/var/www/ood/public/doc}
 SLAC_SDF_DOCS_UPDATE_INTERVAL=${SLAC_SDF_DOCS_UPDATE_INTERVAL:-300}
 EMPTYDIR_MOUNT_PATH=/sdf-docs
 
+# always ensure there's some documentation, otherwise exit
 if [ ! -d "${SLAC_SDF_DOCS_PATH}" ] ; then
     mkdir -p ${SLAC_SDF_DOCS_PATH} && cd ${SLAC_SDF_DOCS_PATH}
-    git clone ${SLAC_SDF_DOCS_REPO}    
+    git clone ${SLAC_SDF_DOCS_REPO}
+    cp -r ${SLAC_SDF_DOCS_PATH}/sdf-docs/. ${EMPTYDIR_MOUNT_PATH}
+else
+    exit 1
 fi
 
-cd ${SLAC_SDF_DOCS_PATH}/sdf-docs
+cd ${EMPTYDIR_MOUNT_PATH}
 git checkout ${SLAC_SDF_DOCS_RELEASE}
 git fetch origin
-git pull origin ${SLAC_SDF_DOCS_RELEASE}
-cp -r ${SLAC_SDF_DOCS_PATH}/sdf-docs/. ${EMPTYDIR_MOUNT_PATH}
 
 while :
 do
