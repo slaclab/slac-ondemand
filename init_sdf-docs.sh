02a5c8725f896187f28890187d9534f076b69989
yee379 <yee379@gmail.com>
2022-03-08 15:03:59 -0800

    feat: update interval as ENV

--- init_sdf-docs.sh
+++ init_sdf-docs.sh
@@ -3,7 +3,7 @@
 SLAC_SDF_DOCS_REPO=${SLAC_SDF_DOCS_REPO:-https://github.com/slaclab/sdf-docs.git}
 SLAC_SDF_DOCS_RELEASE=${SLAC_SDF_DOCS_VERSION:-master}
 SLAC_SDF_DOCS_PATH=/var/www/ood/public/doc
-SLAC_SDF_DOCS_UPDATE_INTERVAL=3600
+SLAC_SDF_DOCS_UPDATE_INTERVAL=${SLAC_SDF_DOCS_UPDATE_INTERVAL:-3600}
 EMPTYDIR_MOUNT_PATH=/sdf-docs
 
 if [ ! -d "${SLAC_SDF_DOCS_PATH}" ] ; then
@@ -19,7 +19,7 @@ cp -r ${SLAC_SDF_DOCS_PATH}/sdf-docs/. ${EMPTYDIR_MOUNT_PATH}
 
 while :
 do
-  echo "$(date '+%Y-%m-%d %H%M.%S %z%Z') Populating SDF docs"
+  echo "$(date '+%Y-%m-%d %H%M.%S %z%Z') Updating SDF docs"
   git pull origin ${SLAC_SDF_DOCS_RELEASE}
   sleep ${SLAC_SDF_DOCS_UPDATE_INTERVAL}
 done
