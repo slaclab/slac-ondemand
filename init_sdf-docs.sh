6461682ea28e8a97854e62ea27ea7161b355784e
Patrick James Pascual <ppascual@cent7a.slac.stanford.edu>
2022-03-08 17:36:31 -0800

    Refactor: check to see if sdf-docs repo exists in emptyDir mount, populate if not.

--- init_sdf-docs.sh
+++ init_sdf-docs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash -ex
 
 SLAC_SDF_DOCS_REPO=${SLAC_SDF_DOCS_REPO:-https://github.com/slaclab/sdf-docs.git}
 SLAC_SDF_DOCS_RELEASE=${SLAC_SDF_DOCS_VERSION:-master}
@@ -6,16 +6,14 @@ TMPDIR=${TMPDIR:-/stage}
 SLAC_SDF_DOCS_UPDATE_INTERVAL=${SLAC_SDF_DOCS_UPDATE_INTERVAL:-300}
 SLAC_SDF_DOCS_PATH=${SLAC_SDF_DOCS_PATH:-/sdf-docs}
 
-# always ensure there's some documentation, otherwise exit
-if [ -d "${SLAC_SDF_DOCS_PATH}" ] ; then
-    mkdir -p ${TMPDIR} && cd ${TMPDIR}
-    git clone ${SLAC_SDF_DOCS_REPO}
+# always ensure there's some documentation
+cd ${SLAC_SDF_DOCS_PATH}
+git status
+STATUS=$?
+if [ $STATUS -gt 0 ]; then
     cp -r ${TMPDIR}/sdf-docs/. ${SLAC_SDF_DOCS_PATH}
-else
-    exit 1
 fi
 
-cd ${SLAC_SDF_DOCS_PATH}
 git checkout ${SLAC_SDF_DOCS_RELEASE}
 git fetch origin
 
