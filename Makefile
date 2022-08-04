706e86373d22055ec8fa5f50a4bca8323ac69b3b
yee379 <yee379@gmail.com>
2022-08-04 14:50:36 -0700

    feat: generic git clone pod

--- Makefile
+++ Makefile
@@ -9,3 +9,8 @@ docker:
 docs:
 	$(RUNTIME) build . -f Dockerfile.docs -t slaclab/sdf-docs:$(TAG)
 	$(RUNTIME) push slaclab/sdf-docs:$(TAG)
+
+gitclone:
+	$(RUNTIME) build . -f Dockerfile.gitclone -t slaclab/gitclone:$(TAG)
+	$(RUNTIME) push slaclab/gitclone:$(TAG)
+
---
+++ entrypoint-gitclone.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+GIT_REPO=${GIT_REPO}
+GIT_RELEASE=${GIT_RELEASE:-master}
+GIT_UPDATE_INTERVAL=${GIT_UPDATE_INTERVAL:-300}
+
+# always ensure there's some documentation
+cd /app
+git status
+STATUS=$?
+if [ $STATUS -gt 0 ]; then
+    git clone ${GIT_REPO} .
+fi
+
+# do some funky stuff with git remove here
+
+git checkout ${GIT_RELEASE} .
+git fetch origin
+
+while :
+do
+  echo "$(date '+%Y-%m-%d %H%M.%S %z%Z') Updating git repo ${GIT_REPO}:${GIT_VERSION}"
+  git pull origin ${GIT_RELEASE}
+  sleep ${GIT_UPDATE_INTERVAL}
+done
