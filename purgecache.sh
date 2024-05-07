d7bd075e6f2c575eb8ac081946bfc944395ef61a
yee379 <yee379@gmail.com>
2024-05-07 15:18:51 -0700

    fix: typo

--- purgecache.sh
+++ purgecache.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-/usr/bin/grep missing /var/lib/ondemand-nginx/config/puns/*.conf  2>/dev/null | /usr/bin/grep rewrite | /usr/bin/sed -e 's|/var/lib/ondemand-nginx/config/puns/||g' -e 's|.conf.*||g' | /usr/bin/xargs -n1 -r -I{} /opt/ood/nginx_stage/sbin/nginx_stage pun --skip-nginx --user {} \&\& /opt/ood/nginx_stage/sbin/nginx_stage nginx --signal reload --user {}
+/usr/bin/grep missing /var/lib/ondemand-nginx/config/puns/*.conf  2>/dev/null | /usr/bin/grep rewrite | /usr/bin/sed -e 's|/var/lib/ondemand-nginx/config/puns/||g' -e 's|.conf.*||g' | /usr/bin/xargs -n1 -r -I% echo '/opt/ood/nginx_stage/sbin/nginx_stage pun --skip-nginx --user % && /opt/ood/nginx_stage/sbin/nginx_stage nginx --signal reload --user %' | sh
+
 
